0310cd98a79226c8750b93c3684c2c5d3a7b2ce9
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2025-08-29 16:26:39 +1200

    Add placeholder React page for War game

---
+++ war.js
@@ -0,0 +1,11 @@
+// Placeholder React component for the War game.
+function WarPlaceholder() {
+  return (
+    <div className="container">
+      <h1 className="title">War</h1>
+      <p>The War game will be available soon.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<WarPlaceholder />, document.getElementById('root'));
