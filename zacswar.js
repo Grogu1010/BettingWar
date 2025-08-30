93fefcbd375954afcaf8b1c94ac402f2cdbdb349
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2025-08-30 14:35:27 +1200

    Update zacswar.js

--- zacswar.js
+++ zacswar.js
@@ -3,6 +3,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const cardw = 500;
+const cardh = 726;
 const cac = new Image(); cac.src = 'cards/ace_of_clubs.png';
 const cad = new Image(); cad.src = 'cards/ace_of_diamonds.png';
 const cah = new Image(); cah.src = 'cards/ace_of_hearts.png';
@@ -10,6 +12,6 @@ const cas = new Image(); cas.src = 'cards/ace_of_spades.png';
 
 
 function a(){
-  ctx.drawImage(cas, 0, 0, 100, 200);
+  ctx.drawImage(cas, 0, 0, carw * 0.25, cardh * 0.25);
 }
 setTimeout(a, 1000);
