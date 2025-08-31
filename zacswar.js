7b917f942f18591af2d828b5142ab3e68e7077b7
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2025-08-31 15:00:48 +1200

    Update zacswar.js

--- zacswar.js
+++ zacswar.js
@@ -32,7 +32,12 @@ class Card{
   }
 }
 
-var card1 = new Card(0, cas, 0, 0, 1);
+const card1 = new Card(0, cas, 0, 0, 1);
+card1.number = 0;
+card1.image = cas;
+card1.posX = 0;
+card1.posY = 0;
+card1.scale = 1;
 
 function a(){
   card1.flipCard();
