3de5136e64cc924cc955d67c1efddc5913bfa02f
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2025-09-06 09:24:26 +1200

    Update zacswar.js

--- zacswar.js
+++ zacswar.js
@@ -77,13 +77,13 @@ function draw(){
 }
 function reset(){
   if(players == 2){
-    sprites[0] = new Card(cas, canvas.width / 2, canvas.width * 0.1, 0.15, 0.15, 180, false);
-    sprites[1] = new Card(cac, canvas.width / 2, canvas.width * 0.6, 0.15, 0.15, 0, false);
+    sprites[0] = new Card(cas, canvas.width / 2, canvas.height * 0.1, 0.15, 0.15, 180, false);
+    sprites[1] = new Card(cac, canvas.width / 2, canvas.height * 0.9, 0.15, 0.15, 0, false);
   }
   else if(players == 3){
-    sprites[0] = new Card(cas, canvas.width / 2, canvas.width * 0.1, 0.15, 0.15, 180, false);
-    sprites[1] = new Card(cac, canvas.width / 2, canvas.width * 0.6, 0.15, 0.15, 0, false);
-    sprites[2] = new Card(cac, canvas.width / 2, canvas.width * 0.6, 0.15, 0.15, 0, false);
+    sprites[0] = new Card(cas, canvas.width / 2, canvas.height * 0.1, 0.15, 0.15, 180, false);
+    sprites[1] = new Card(cac, canvas.width / 2, canvas.height * 0.9, 0.15, 0.15, 0, false);
+    sprites[2] = new Card(cac, canvas.width / 2, canvas.height * 0.9, 0.15, 0.15, 0, false);
   }
   else if(player == 4){
     sprites[0] = new Card(cas, canvas.width / 2, canvas.height * 0.1, 0.15, 0.15, 180, false);
