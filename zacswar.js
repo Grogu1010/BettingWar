8ba2d76554dd8a6169cfc863cdee7de1155f0677
Gamemaker271 <119999930+Gamemaker271@users.noreply.github.com>
2025-09-09 11:12:06 +1200

    Update zacswar.js

--- zacswar.js
+++ zacswar.js
@@ -47,8 +47,8 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const dpr = window.devicePixelRatio || 1;
-const currentCanvasWidth = 800;
-const currentCanvasHeight = 550;
+const baseCanvasWidth = 800;
+const baseCanvasHeight = 550;
 canvas.style.width = baseCanvasWidth + 'px';
 canvas.style.height = baseCanvasHeight + 'px';
 canvas.width = baseCanvasWidth * dpr;
